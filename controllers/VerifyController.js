658748312b300f24132a852531dad3747aeae02b
Kel-D-Coder <okolochibundu887@gmail.com>
2025-07-15 20:29:50 +0100

    added some more endpoint

--- controllers/VerifyController.js
+++ controllers/VerifyController.js
@@ -1,37 +1,81 @@
+const axios= require('axios');
 const {plateVerificationScrape, vinVerificationScrape} = require('../utils/scrape');
+const http = require('http-status-codes')
+const crypto = require('crypto');
+const sha1 = require('sha1');
 
 const VerifyPlate = async (req, res) => {
-    const { plateNumber } = req.body;
-    const url = 'https://nvis.frsc.gov.ng/VehicleManagement/VerifyPlateNo';
-    if (!plateNumber) return res.status(400).json({ error: 'Plate number required' });
+
+    if (!req.body.vehicle_number) {
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({msg: 'Vehicle Number is required'});
+    }
 
     try {
-        const result = await plateVerificationScrape(url, plateNumber)
+        const response = await axios.post("https://api.prembly.com/identitypass/verification/vehicle",
+            {vehicle_number: req.body.vehicle_number}, {
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": process.env.PREMBLY_API_KEY,
+                "app_id": process.env.PREMBLY_APP_ID
+            }
+        })
 
-        if (result === null) {
-            return res.status(404).json({ errorMsg: 'No data found for the provided plate number' });
-        }
+        const data = await response.data;
 
-        return res.status(200).json(result);
-
-        
+        return res.status(http.StatusCodes.OK).json(data);
     } catch (error) {
-        res.status(500).json({ errorMsg: error.message });
-        console.log(error);
+        console.log(error.message);
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({msg: "Error Occurred"})
     }
 
    
 }
 
-const VinVerify = async (req, res) => { 
+const VinVerify = async (req, res) => {
     const {vin} = req.body;
-    // console.log(vin)
-    if (!vin) return res.status(400).json({ error: 'VIN required' });
+    console.log(vin)
+    if (!vin) return res.status(http.StatusCodes.BAD_REQUEST).json({ error: 'VIN required' });
    try {
-     const result = await vinVerificationScrape('https://autovin.com.ng/', vin);
+       const response = await axios.post("https://api.prembly.com/identitypass/verification/vehicle/vin",
+       { vin }, {
+           headers: {
+               "Content-Type": "application/json",
+               "x-api-key": process.env.PREMBLY_API_KEY,
+               "app_id": process.env.PREMBLY_APP_ID
+           }
+       })
+
+       const data = await response.data;
+
+       console.log(data)
+
+       return res.status(http.StatusCodes.OK).json(data);
    } catch (error) {
-    
+       console.log(error.message);
+    return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Error Occurred" })
    }
 }
 
-module.exports = {VerifyPlate, VinVerify};
+const VehicleHistory = async (req, res) => {
+    const {vin} = req.body;
+
+    if (!vin) return res.status(http.StatusCodes.BAD_REQUEST).json({ error: 'VIN required' });
+    try {
+        const response = await axios.get(`https://api.vehicledatabases.com/htmlreport/${vin}`, {
+            headers: {
+                "x-AuthKey": "864f609255f911f096100242ac120002"
+            }
+        })
+
+        const data = await response.data;
+
+        console.log(data)
+
+        return res.status(http.StatusCodes.OK).json(data);
+    } catch(error) {
+        console.log(error);
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({msg: "Record(s) were not found for this vehicle."});
+    }
+}
+
+module.exports = { VerifyPlate, VinVerify, VehicleHistory };
--- routes/VerificationRoute.js
+++ routes/VerificationRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {VerifyPlate, VinVerify} = require("../controllers/VerifyController");
+const {VerifyPlate, VinVerify, StolenCheck, VehicleHistory} = require("../controllers/VerifyController");
 
 router.post('/verify-plate', VerifyPlate)
 router.post('/verify-vin', VinVerify)
+// router.post('/stolen-check', StolenCheck)
+router.post('/vehicle-history', VehicleHistory);
 
 module.exports = router
