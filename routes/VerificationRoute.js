982ec613fb2176c8168d8f3fb8b6f9745e637f2e
Kel-D-Coder <okolochibundu887@gmail.com>
2025-07-17 13:51:43 +0100

    added an enpoint

--- routes/VerificationRoute.js
+++ routes/VerificationRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {VerifyPlate, VinVerify, StolenCheck, VehicleHistory} = require("../controllers/VerifyController");
+const {VerifyPlate, VinVerify, VehicleHistory, MarketValue} = require("../controllers/VerifyController");
 
 router.post('/verify-plate', VerifyPlate)
 router.post('/verify-vin', VinVerify)
-// router.post('/stolen-check', StolenCheck)
 router.post('/vehicle-history', VehicleHistory);
+router.post('/market-value', MarketValue);
 
 module.exports = router
